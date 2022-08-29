c8fdbfd5387733818601013db00022c60da420a4
nduitsman <nduitsman@gmail.com>
2022-08-29 17:17:04 -0400

    styling on new post page

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -53,7 +53,6 @@ router.get('/:id', async (req, res) => {
 });
 // DESTROY / DELETE  - localhost:4000/posts/<_id>
 router.delete('/:id', async (req,res) => {
-    console.log("Are we seeing this")
     try{
        const deletePost = await db.Posts.findByIdAndDelete(req.params.id); 
        return res.redirect('/posts');
