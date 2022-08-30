b9312794f5fa12f608bce83e577ab522c2b590e2
DixonAMichael <dixonamichael@gmail.com>
2022-08-30 11:00:09 -0400

    Troubleshooting Post Register Route

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -10,43 +10,82 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 // MODEL IMPORT
-const user = require('../models');
+const {User} = require('../models');
+console.log(User);
 
 // GET LOGIN ROUTE 
 router.get('/login', (req, res) => {
     res.render('users/login.ejs');
 });
 
+// GET REGISTER ROUTE
+router.get('/register', (req, res) => {
+    res.render('users/register.ejs');
+});
+
 // POST LOGIN ROUTE
-router.post('/login', async (req,res, next) => {
-    try{
+router.post('/login', async (req, res, next) => {
+    try {
         let formData = req.body;
         // this checks the boolean if the user exists
-        let foundUser = await user.findOne({email: formData.email});
+        let foundUser = await User.findOne({ email: formData.email });
         if (!foundUser) {
             return res.redirect('/register')
-        }else{
+        } else {
             const match = await bcrypt.compare(formData.password, foundUser.password);
-            if(!match) return res.send(`email or password doesn't match` );
+            if (!match) return res.send(`email or password doesn't match`);
             req.session.currentUser = {
                 id: foundUser._id,
                 username: foundUser.username,
             }
             return res.redirect('/posts');
         }
-    }catch(error){
-    console.log(error);
-    next("this is broken");
+    } catch (error) {
+        console.log(error);
+        next("this is broken");
     }
 });
 
-// GET REGISTER ROUTE
+
 
 
 // POST REGISTER
+router.post('/register', async (req, res, next) => {
+    console.log("hi")
+    try {
+        let formData = req.body;
+        console.log(`created ${formData}`)
+        let foundUser = await User.exists({ email: formData.email });
+        if (foundUser) {
+            return res.redirect('/login')
+        } else {
+            let rounds = parseInt(process.env.SALT_ROUNDS)
+            let salt = await bcrypt.genSalt(rounds);
+            let hash = await bcrypt.hash(formData.password, salt);
+            formData.password = hash;
+
+            const newUser = await User.create(formData);
+            console.log(`create ${newUser}`)
+            return res.redirect('/')
+        }
 
 
+    } catch (error) {
+        console.log(error);
+        next("this is broken");
+    }
+});
 
+// GET LOGOUT
+router.get("/logout", async function (req, res) {
+    try {
+        await req.session.destroy();
+        return res.redirect("/login");
+    } catch (error) {
+        console.log(error);
+        return res.send(error);
+    }
+});
 
 
 
--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema(
     {
-        Email: { type: String, required: [true, "Email cannot be empty"] },
+        email: { type: String, required: [true, "Email cannot be empty"] },
         password: { type: String, required: [true, "Password cannot be empty"] },
-        Username: { type: String, required: [true, "Username cannot by empty"]}
+        username: { type: String, required: [true, "Username cannot by empty"]}
     },
     { timestamps: true }
 );
