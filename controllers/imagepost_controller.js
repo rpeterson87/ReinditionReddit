e09c5aed59b8210e36f704ad66479ee8782ee37b
rpeterson87 <oshorhino5053@gmail.com>
2022-09-04 12:28:03 -0400

    Setup image post controller and exports

---
+++ controllers/imagepost_controller.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const session = require('express-session');
+const { Posts } = require('../models');
+const router = express.Router();
+const fs = require('fs')
+const path = require('path')
+const multer = require('multer')
+
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, __dirname + '/uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + new Date().toISOString())
+    }
+});
+  
+let upload = multer({ storage: storage });
+
+
+// MIDDLEWARE
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+
+
+// MODEL IMPORT
+const db = require('../models');
+const { debugPort } = require('process');
+
+
+
+router.get('/new', (req, res) => {
+    try {
+        if (req.session.currentUser) {
+
+            const session = req.session;
+            context = { session: session }
+            res.render('new.ejs', context)
+        } else {
+            res.redirect('/login');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+router.post('/', upload.single('img'), (req, res, next) => {
+    if (req.file){
+    const obj = {
+        title: req.body.title,
+        community: req.body.community,
+        body: req.body.body,
+        
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+        
+    }
+    console.log(req.file)
+    db.Posts.create(obj, (err, item) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // item.save();
+            // res.send(obj)
+            res.redirect('/');
+        }
+    });
+}else{
+    const obj = {
+        title: req.body.title,
+        community: req.body.community,
+        body: req.body.body,
+    }
+    console.log(req.file)
+    db.Posts.create(obj, (err, item) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // item.save();
+            // res.send(obj)
+            res.redirect('/');
+        }
+    });
+}
+});
+
+
+
+module.exports = router
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     posts: require('./post_controller'),
-    users: require('./user_controllers')
+    users: require('./user_controllers'),
+    image: require('./imagepost_controller')
 };
